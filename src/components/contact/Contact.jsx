f84d9f7bedd879b457bb89f737be3d8d5a24c9ca
Eric Martin <eric_martin@me.com>
2022-01-30 15:09:23 -0700

    created toggle button functionality

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,12 +3,15 @@ import './contact.css';
 import Phone from '../../img/phone.png';
 import Email from '../../img/email.png';
 import Address from '../../img/address.png';
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import emailjs from "@emailjs/browser";
+import { ThemeContext } from '../../context';
 
 const Contact = () => {
     const formRef = useRef();
     const [done, setDone] = useState(false);
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
     const handleSubmit = (e) => {
         e.preventDefault();
         emailjs.sendForm('service_d08tomv', 'template_s23y5ez', formRef.current, 'user_tNvlABxMkv2wwnLgNxS4f')
@@ -58,10 +61,10 @@ const Contact = () => {
                   <b>What's your story?</b> Get in touch. Always freelancing if the right project comes along. me.
               </p>
               <form ref={formRef} onSubmit={handleSubmit}>
-                  <input type="text" placeholder="Name" name="user_name" />
-                  <input type="text" placeholder="Subject" name="user_subject" />
-                  <input type="text" placeholder="Email" name="user_email" />
-                  <textarea rows="5" placeholder="Message" name="message"/>
+                  <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="user_name" />
+                  <input style={{ backgroundColor: darkMode && "#333" }}  type="text" placeholder="Subject" name="user_subject" />
+                  <input style={{ backgroundColor: darkMode && "#333" }}  type="text" placeholder="Email" name="user_email" />
+                  <textarea style={{ backgroundColor: darkMode && "#333" }}  rows="5" placeholder="Message" name="message"/>
                   <button>Submit</button>
                   {done && "Thank you..."}
               </form>
