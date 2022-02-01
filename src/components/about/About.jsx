754640e813adeb043177c899b6d30b39f7cbdb99
Eric Martin <eric_martin@me.com>
2022-01-31 18:14:50 -0700

    updated about.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -35,42 +35,6 @@ const About = () => {
           <p className="a-desc">
             Interested in the entire stack of development and working on ambitious projects with positive people.
           </p>
-          {/* <div className="a-tech"> */}
-            {/* <img src={Award} alt="" className="a-award-img" /> */}
-            {/* <div className="a-tech-texts">
-              <h2 style={{ color:darkMode ? "white" : '#555' }}className="a-tech-title">Technologies</h2>
-              <div>
-                <ul className="a-tech-desc">
-                  <h3 className="a-tech-lang">Languages</h3>
-                  <li className="a-tech-item">HTML5</li>
-                  <li className="a-tech-item">CSS3</li>
-                  <li className="a-tech-item">JavaScript</li>
-                  <li className="a-tech-item">SQL</li>
-                </ul>
-                <ul className="a-tech-desc">
-                  <h3 className="a-tech-db">Databases</h3>
-                  <li className="a-tech-item">MySQL</li>
-                  <li className="a-tech-item">MongoDB</li>
-                  <li className="a-tech-item">Sequelize</li>
-                  <li className="a-tech-item">Mongoose</li>
-                </ul>
-                <ul className="a-tech-desc">
-                  <h3 className="a-tech-frame">Frameworks</h3>
-                  <li className="a-tech-item">React</li>
-                  <li className="a-tech-item">Bootstrap</li>
-                  <li className="a-tech-item">Bulma</li>
-                  <li className="a-tech-item">jQuery</li>
-                </ul>
-                <ul className="a-tech-desc">
-                  <h3 className="a-tech-servers">Servers</h3>
-                  <li className="a-tech-item">Node</li>
-                  <li className="a-tech-item">Express</li>
-                  <li className="a-tech-item">Rest</li>
-                  <li className="a-tech-item"></li>    
-                </ul>
-              </div>
-            </div>
-          </div> */}
       </div>
     </div>
   );
