6919f74a99c180f02ed9370be50dd48f4c301ec5
Eric Martin <eric_martin@me.com>
2022-02-01 23:59:09 -0700

    added validation and began working on Resume page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
       <div className="a-right">
         <h1 className="a-title">About Me</h1>
           <p className="a-sub">
-            It is a long established fact that a reader will be distracted by the readable content.
+            "I'm not superstitious, but I am a little stitious." <span className="a-quote">- Michael Scott</span>
           </p>
           <p className="a-desc">
             My name is Eric Martin. I'm a Full Stack Developer located in Denver, CO. I have a passion for intuitive websites. I have over 10 years of customer service experience that I bring to each and every project. I truly am an end-user focused developer.
@@ -23,12 +23,44 @@ const About = () => {
           <p className="a-desc">
             I'm a fan of video games, D&D, pretty much every sport, and of course...movies!
           </p>
-          <p className="a-desc">
+          <p className="a-desc a-mobile">
             I utilize the MERN stack for all of my projects: (MongoDB/MySQL, Express, React, Node)
           </p>
-          <p className="a-desc">
+          <p className="a-desc a-mobile">
             Interested in the entire stack of development and working on ambitious projects with positive people.
           </p>
+          <h2>Technologies</h2>
+          <ul className="a-tech-list">
+              <span className="a-tech-list-title">Languages:</span>
+              <li className="a-tech-list-item">HTML</li>
+              <li className="a-tech-list-item">CSS</li>
+              <li className="a-tech-list-item">JavaScript</li>
+              <li className="a-tech-list-item">SQL</li>
+              <li className="a-tech-list-item">MongoDb</li>
+              <li className="a-tech-list-item">GraphQL</li>
+              <li className="a-tech-list-item">Node.js</li>
+          </ul>
+          <ul className="a-tech-list">
+              <span className="a-tech-list-title">Libraries:</span>
+              <li className="a-tech-list-item">ReactJS</li>
+              <li className="a-tech-list-item">Bootstrap</li>
+              <li className="a-tech-list-item">jQuery</li>
+              <li className="a-tech-list-item">Node</li>
+              <li className="a-tech-list-item">Handlebars</li>
+              <li className="a-tech-list-item">Sequelize</li>
+              <li className="a-tech-list-item">Mongoose</li>
+              <li className="a-tech-list-item">Bulma</li>
+          </ul>
+          <ul className="a-tech-list">
+              <span className="a-tech-list-title">Applications:</span>
+              <li className="a-tech-list-item">GitHub</li>
+              <li className="a-tech-list-item">Heroku</li>
+              <li className="a-tech-list-item">GitLab</li>
+              <li className="a-tech-list-item">NPM</li>
+              <li className="a-tech-list-item">Compass</li>
+              <li className="a-tech-list-item">dBeaver</li>
+              <li className="a-tech-list-item">Insomnia</li>
+          </ul>
       </div>
     </div>
   );
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import './contact.css';
-import Phone from '../../img/phone.png';
-import Email from '../../img/email.png';
-import Address from '../../img/address.png';
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import isEmail from 'validator/lib/isEmail';
 import * as MdIcons from "react-icons/md";
 import * as HiIcons from "react-icons/hi";
 import * as BiIcons from "react-icons/bi";
@@ -12,6 +10,12 @@ import * as BiIcons from "react-icons/bi";
 const Contact = () => {
     const formRef = useRef();
     const [done, setDone] = useState(false);
+    const [error, setError] = useState("");
+    const [checkName, setNameCheck] = useState("Name");
+    const [checkSubject, setSubjectCheck] = useState("Subject");
+    const [checkEmail, setEmailCheck] = useState("Email");
+    const [checkMessage, setMessageCheck] = useState("Message");
+
     const handleSubmit = (e) => {
         e.preventDefault();
         emailjs.sendForm('service_d08tomv', 'template_s23y5ez', formRef.current, 'user_tNvlABxMkv2wwnLgNxS4f')
@@ -23,6 +27,27 @@ const Contact = () => {
             console.log(error.text);
         });
     };
+
+    const handleName = (e) => {
+        e.preventDefault();
+        (!e.target.value.length) ? setNameCheck("Name is required") : setNameCheck("Name");
+    }
+    const handleSubject = (e) => {
+        e.preventDefault();
+        (!e.target.value.length) ? setSubjectCheck("Subject is required") : setSubjectCheck("Subject");
+    }
+    const handleEmail = (e) => {
+        e.preventDefault();
+        (!e.target.value.length) ? setEmailCheck("Email is required") : setEmailCheck("Email");
+    }
+    const handleMessage = (e) => {
+        e.preventDefault();
+        (!e.target.value.length) ? setMessageCheck("Message is required") : setMessageCheck("Message");
+    }
+    const handleChange = (e) => {
+        e.preventDefault();
+        isEmail(e.target.value) ? setError("") : setError("Please enter a valid email");
+    }
 
   return (
   <div className="c contact">
@@ -56,12 +81,17 @@ const Contact = () => {
                   <b>Like what you see? Reach out!</b> Please complete the form below to get in touch with me.
               </p>
               <form ref={formRef} onSubmit={handleSubmit}>
-                  <input className="input-name" type="text" placeholder="Name" name="user_name" />
-                  <input className="input-subject" type="text" placeholder="Subject" name="user_subject" />
-                  <input className="input-email" type="text" placeholder="Email" name="user_email" />
-                  <textarea className="vertical" rows="5" placeholder="Message" name="message"/>
+                  <input className="input-name" type="text" placeholder={checkName} name='{user_name' onBlur={handleName} className={checkName === "Name is required" ? "inactive" : "active"} required/>
+
+                  <input className="input-subject" type="text" placeholder={checkSubject} name={'user_subject'} onBlur={handleSubject} className={checkSubject === "Subject is required" ? "inactive" : "active"} required />
+
+                  <input className="input-email" type="text" placeholder={checkEmail} name={"user_email"} onBlur={handleEmail} onChange={handleChange} className={checkEmail === "Email is required" ? "inactive" : "active"} required />
+                  <p className='errorMessage'>{error}</p>
+
+                  <textarea rows="5" placeholder={checkMessage} name="message" onBlur={handleMessage} className={checkMessage === "Message is required" ? "inactive" : "active"} required className="vertical" />
+
                   <button>Submit</button>
-                  {done && "Thank you..."}
+                  {done && "Thank you. Your email has been sent!"}
               </form>
           </div>
       </div>
